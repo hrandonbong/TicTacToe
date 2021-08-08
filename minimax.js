1a88e7254a1b70198a18cfec27193016960e2fb3
hrandonbong <71863517+hrandonbong@users.noreply.github.com>
2021-08-07 21:25:09 -0700

    Add files via upload

---
+++ minimax.js
@@ -0,0 +1,64 @@
+function bestMove() {
+    // AI to make its turn
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (board[i][j] == '') {
+          board[i][j] = 'O';
+          let score = minimax(board, 0, false);
+          board[i][j] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            move = { i, j };
+          }
+        }
+      }
+    }
+    board[move.i][move.j] = 'O';
+    drawO(move.i,move.j);
+  }
+  
+  let scores = {
+    O: 10,
+    X: -10,
+    tie: 0
+  };
+  
+  function minimax(board, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+      return scores[result];
+    }
+  
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i][j] == '') {
+            board[i][j] = 'O';
+            let score = minimax(board, depth + 1, false);
+            board[i][j] = '';
+            bestScore = Math.max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i][j] == '') {
+            board[i][j] = 'X';
+            let score = minimax(board, depth + 1, true);
+            board[i][j] = '';
+            bestScore = Math.min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
